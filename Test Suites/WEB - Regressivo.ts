da470a63bc8e212fac80631e72e4c4cce947d3e7
Windows 10 <webmaster@rperformancegroup.com>
2023-05-23 12:05:14 -0300

    create test suit

---
+++ Test Suites/WEB - Regressivo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WEB - Regressivo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>140a34e2-97c8-44b6-a5c1-f7dfe971efd4</testSuiteGuid>
+   <testCaseLink>
+      <guid>e5312861-06eb-4d96-84d9-1f541bf5e766</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Buscar Unimed mais próxima</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
