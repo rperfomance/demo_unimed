71ad8cb0e26e32e91a3aa181822f2c49a7aab62c
Windows 10 <webmaster@rperformancegroup.com>
2023-06-23 16:34:34 -0300

    .

---
+++ Object Repository/New Folder/Page_Home - Site Institucional/a_ACESSO RPIDO.rs
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_ACESSO RPIDO</name>
+   <tag></tag>
+   <elementGuidId>df3317b1-ec99-4173-9c92-df34fede02cd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='acesso-rapido-click']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#acesso-rapido-click</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>0091addc-a00d-4f8e-b5da-a979be615745</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>nav-link menu-principal__nivel-2--bold menu-principal--cor-preta p-0</value>
+      <webElementGuid>95e292bc-d39c-42c7-b0a9-2ef91a6a1d22</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-toggle</name>
+      <type>Main</type>
+      <value>collapse</value>
+      <webElementGuid>a61d2da3-62e1-4ed4-9e5b-7aca5781bc69</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>#collapseAcessoRapido</value>
+      <webElementGuid>a83aa709-de84-47fc-b893-d9072a029685</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>a4749f15-cbd1-4d5b-b729-4bad64edb7e8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-expanded</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>8c7b8028-fcac-4847-a08d-cb2c4a67715f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-controls</name>
+      <type>Main</type>
+      <value>collapseAcessoRapido</value>
+      <webElementGuid>e8130dbf-a7bd-4839-95ae-084fe935c7d0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>acesso-rapido-click</value>
+      <webElementGuid>bbe10a28-a0c7-48f9-8a08-80a0d768eed9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                        ACESSO RÁPIDO
+                                        
+                                    
+                                    
+                                
+                                    </value>
+      <webElementGuid>aa4e1569-bcbc-4a18-9dd0-11aa36362d3f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;acesso-rapido-click&quot;)</value>
+      <webElementGuid>93a89be8-e57a-429e-b026-fe4578370a57</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@id='acesso-rapido-click']</value>
+      <webElementGuid>56a98ae9-0e41-46ba-8edb-c8ac218a76a8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//li[@id='li-acesso-rapido']/a</value>
+      <webElementGuid>38f0c62e-fdf5-4e2a-ad88-26acbbaccfc8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'ACESSO RÁPIDO')])[2]</value>
+      <webElementGuid>6e4a1dd1-da0d-4c00-acd9-ce5a02f1a4ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ENCONTRE UM MÉDICO'])[1]/following::a[1]</value>
+      <webElementGuid>300e34b1-5fe4-4bdd-83c6-9a01a366ebfa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Imprensa'])[2]/following::a[2]</value>
+      <webElementGuid>4cd42e2a-fac8-4eb5-aafe-ed0c91d283c6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sistema Unimed'])[3]/preceding::a[2]</value>
+      <webElementGuid>ebf5d2f4-6d90-4ec2-a103-2ac7a1808c39</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Unimed do Brasil'])[3]/preceding::a[3]</value>
+      <webElementGuid>1a90d24a-2519-4296-9dd4-7b7ff8d9135e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '#collapseAcessoRapido')])[2]</value>
+      <webElementGuid>bcbb2042-442d-4576-90a7-ddafcd979168</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ul[2]/li[2]/a</value>
+      <webElementGuid>96d3dad3-8619-4f5f-b353-ca76decfc055</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '#collapseAcessoRapido' and @id = 'acesso-rapido-click' and (text() = '
+                                        ACESSO RÁPIDO
+                                        
+                                    
+                                    
+                                
+                                    ' or . = '
+                                        ACESSO RÁPIDO
+                                        
+                                    
+                                    
+                                
+                                    ')]</value>
+      <webElementGuid>23c19f28-57d8-4bb1-99cb-01c68868e895</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/New Folder/Page_Home - Site Institucional/span_ENCONTRE UM MDICO.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_ENCONTRE UM MDICO</name>
+   <tag></tag>
+   <elementGuidId>5234798e-3b7f-4a5d-9502-9b46746fe15a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='accordion']/ul[2]/li/a/span</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.nav-link.menu-principal__nivel-2--bold.menu-principal--cor-preta.p-0 > span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>f81fa68e-d982-4190-a4e9-b0dcc49cdf4e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>ENCONTRE UM MÉDICO</value>
+      <webElementGuid>c7ffdf14-555d-4907-8de5-8d17dc9a1b31</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;accordion&quot;)/ul[@class=&quot;flex-lg-row flex-md-column menu-principal__nivel-2 mt-0 navbar-nav hidden-sm hidden-xs area-atalho-menu&quot;]/li[@class=&quot;nav-item menu-principal__nivel-2--nav-item&quot;]/a[@class=&quot;nav-link menu-principal__nivel-2--bold menu-principal--cor-preta p-0&quot;]/span[1]</value>
+      <webElementGuid>6b6dc416-3e2e-4da7-b1b4-84f3cb289995</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='accordion']/ul[2]/li/a/span</value>
+      <webElementGuid>b8143f0c-87a0-4582-9c39-210b62d81df1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Imprensa'])[2]/following::span[1]</value>
+      <webElementGuid>04b1253e-b851-4915-a2c2-73230b2bc154</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Releases'])[2]/following::span[2]</value>
+      <webElementGuid>69a322bb-15e1-4023-a90c-f2f745c780a2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ACESSO RÁPIDO'])[2]/preceding::span[1]</value>
+      <webElementGuid>0e20b226-ea10-4059-b1a4-bc46431f37b2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sistema Unimed'])[3]/preceding::span[3]</value>
+      <webElementGuid>3d641cdb-97ad-42fb-acce-ed07ae49bf98</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ul[2]/li/a/span</value>
+      <webElementGuid>3f3a9299-30a5-4ac9-97f5-fdec4418bb8c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'ENCONTRE UM MÉDICO' or . = 'ENCONTRE UM MÉDICO')]</value>
+      <webElementGuid>a26e7632-35d6-4acf-8019-fde6ebf6ea38</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
