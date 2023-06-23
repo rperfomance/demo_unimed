1a2bce2d9b346d13b7a3709d695f5868c5915956
Windows 10 <webmaster@rperformancegroup.com>
2023-06-23 16:26:32 -0300

    add test

--- Test Suites/WEB - Regressivo.ts
+++ Test Suites/WEB - Regressivo.ts
@@ -15,7 +15,28 @@
       <guid>e5312861-06eb-4d96-84d9-1f541bf5e766</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/WEB/Buscar Unimed mais próxima</testCaseId>
+      <testCaseId>Test Cases/WEB/Feature XPTO/Buscar Unimed mais próxima</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d7c7063b-106b-4226-b29b-66c5ce923bfc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Feature XPTO/Abrir site</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5926fa11-8893-42b3-b6ed-1185d912da85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Buscar piada</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c4b92aa-fcae-416c-9d30-dd9646cdba0f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Feature XPTO/Reutilizar teste</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
